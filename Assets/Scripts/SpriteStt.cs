bd2579d28b0d290443b73bccb197463d7687cee4
akashjha011 <akash.jsmith@gmail.com>
2021-06-13 20:17:01 +0530

    Initial commit

---
+++ Assets/Scripts/SpriteStt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteStt : MonoBehaviour
+{
+    [SerializeField] private SpriteState spriteState;
+
+    [SerializeField] private Button button;
+
+    public void SpriteChange()
+    {
+        button.spriteState = spriteState;
+    }
+
+}
---
+++ Assets/Scripts/Toggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Toggle : MonoBehaviour
+{
+    [SerializeField] private Sprite[] buttonSprites;
+
+    [SerializeField] private Image targetButton;
+
+    public void ChangeSprite()
+    {
+        if (targetButton.sprite == buttonSprites[0])
+        {
+            targetButton.sprite = buttonSprites[1];
+            return;
+        }
+
+        targetButton.sprite = buttonSprites[0];
+    }
+}
